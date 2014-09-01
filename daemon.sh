944ca3168f8733d95274b706b3e3b321496fe028
Ralph Giles <giles@mozilla.com>
2014-08-31 22:51:45 -0700

    Generate an ssh key for pushing to github.
    We print the public half of the key pair when the daemon
    script is started so we can pull it from the logs and
    authorize the container instance with the remote server.

--- daemon.sh
+++ daemon.sh
@@ -3,6 +3,14 @@
 # Script to continually update the mirror.
 INTERVAL=256
 
+# Generate ssh key pair if necessary.
+SSH_KEY=/mirror/.ssh/id_rsa.pub
+if ! test -f ${SSH_KEY}; then
+  ssh-keygen -N '' -f ${SSH_KEY%.pub}
+fi
+# Print the public key so we can authorize the container.
+cat ${SSH_KEY}
+
 while true; do
   echo [$(date -u)]
   ./update.sh
